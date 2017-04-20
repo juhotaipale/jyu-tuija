656d6d09b7b67c1eef37682b4bc2f0b8fb6f3d7f
Juho Taipale <juho.taipale@gmail.com>
2017-04-20 16:11:00 +0300

    Lisätty valikkoon ylläpitotyökaluille oma valikko

--- layout/nav.php
+++ layout/nav.php
@@ -8,14 +8,24 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.php?page=home">TuIjA</a>
+            <a class="navbar-brand" style="padding-right: 50px;" href="index.php?page=home">TuIjA</a>
         </div>
         <div class="collapse navbar-collapse" id="navbar-collapse">
             <ul class="nav navbar-nav">
-                <li><a href="index.php?page=home" class="active"><?php echo _("Etusivu"); ?></a></li>
-                <li><a href="index.php?page=infra"><?php echo _("Infrastruktuurihaku"); ?></a></li>
-                <li><a href="#"><?php echo _("Aineistohaku"); ?></a></li>
-                <li><a href="#"><?php echo _("Henkilöhaku"); ?></a></li>
+                <li><a href="index.php?page=infra"><?php echo _("Infrastruktuuri"); ?></a></li>
+                <li><a href="#"><?php echo _("Aineisto"); ?></a></li>
+                <li><a href="#"><?php echo _("Osaaminen"); ?></a></li>
+
+                <?php
+                if ($login->loggedIn() && $user->isAdmin()) {
+                    echo "<li class='dropdown'>
+                            <a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>" . _("Ylläpitotyökalut") . " <span class='caret'></span></a>
+                            <ul class='dropdown-menu'>
+                                <li><a href='index.php?page=admin/users'>" . _("Käyttäjät") . "</a></li>
+                            </ul>
+                        </li>";
+                }
+                ?>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <?php
