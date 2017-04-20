66667dfd37584ea20a9f2d747445827c085fa285
Juho Taipale <juho.taipale@gmail.com>
2017-04-20 16:11:41 +0300

    Lisätty monikielituki virhesivuille ja lisätty 403-virhe

--- pages/error/403.php
+++ pages/error/403.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-md-12 text-center">
-        <h1>404</h1>
-        <p><?php echo _("Sivua ei löydy."); ?> <a
+        <h1>403</h1>
+        <p><?php echo _("Ei käyttöoikeutta."); ?> <a
                     onclick="window.history.back();"><?php echo _("Palaa takaisin edelliselle sivulle."); ?></a></p>
     </div>
 </div>
